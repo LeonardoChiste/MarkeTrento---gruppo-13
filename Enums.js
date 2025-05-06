d87260957552be9f93d127fc4629f5f48c892fcc
Leonardo Chiste <leonardochist03@gmail.com>
2025-05-06 10:51:29 +0200

    add enums

---
+++ Enums.js
@@ -0,0 +1,45 @@
+const Stato = {
+    InElaborazione,
+    Spedito,
+    Consegnato,
+    Rifiutato
+}
+const Tannuncio={
+    Evento, Promozione
+}
+const Tazienda = {
+    Ristorante, 
+    Bar,
+    Locale,
+    ProLoco,
+    Altro,
+    Cooperativa,
+    Impresa,
+    Malga,
+    Fattoria,
+    AziendaAgricola,
+    Agricoltore,
+    Allevatore
+}
+const Tprodotto = {
+    Mele,
+    Pere,
+    FruttiBosto,
+    Castagne,
+    Asparagi,
+    Ciliege,
+    Olio,
+    Alcolici,
+    Gastronomia, 
+    Sughi, 
+    Tuberi, 
+    Carne, 
+    Pesce, 
+    Formaggi, 
+    Miele, 
+    Bevande, 
+    Dolci, 
+    Salumi, 
+    Funghi, 
+    AltraFrutta
+}
