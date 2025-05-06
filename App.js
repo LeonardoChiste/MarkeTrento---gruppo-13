bf9374b60833b1c7ce4c07729fa5ea7de0161fff
Leonardo Chiste <leonardochist03@gmail.com>
2025-05-06 11:40:13 +0200

    work please

--- App.js
+++ App.js
@@ -1,54 +1,9 @@
 //commit di Leonardo
-const Stato = {
-    InElaborazione,
-    Spedito,
-    Consegnato,
-    Rifiutato
-}
-const Tannuncio={
-    Evento, Promozione
-}
-const Tazienda = {
-    Ristorante, 
-    Bar,
-    Locale,
-    ProLoco,
-    Altro,
-    Cooperativa,
-    Impresa,
-    Malga,
-    Fattoria,
-    AziendaAgricola,
-    Agricoltore,
-    Allevatore
-}
-const Tprodotto = {
-    Mele,
-    Pere,
-    FruttiBosto,
-    Castagne,
-    Asparagi,
-    Ciliege,
-    Olio,
-    Alcolici,
-    Gastronomia, 
-    Sughi, 
-    Tuberi, 
-    Carne, 
-    Pesce, 
-    Formaggi, 
-    Miele, 
-    Bevande, 
-    Dolci, 
-    Salumi, 
-    Funghi, 
-    AltraFrutta
-}
+//scritto malissimo, cancella tutto 
 class Prodotto{
-    constructor(nome, descrizione,costo){
+    constructor(nome, descrizione){
         this.nome=nome;
         this.descrizione=descrizione;
-        this.prezzo=costo;
     }
 }
 class PagaDati{
