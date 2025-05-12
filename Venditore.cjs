97d1bf7839eb91551e8637818826a7b07c940aa0
Leonardo Chiste <leonardochist03@gmail.com>
2025-05-12 13:44:18 +0200

    divisione in file per frontend

--- Venditore.cjs
+++ Venditore.cjs
@@ -4,8 +4,13 @@ class Venditore extends Imprenditore{
     constructor(name, surname, birthdate, email, password, datiPagamento){
         super(name, surname, birthdate, email, password);
         this.datiPagamento=datiPagamento;
-
+        
     }
-    pubblicaProdotto(prod){ }
-    eliminaProdotto(prod){ }
+    pubblicaProdotto(prod){ 
+         
+    }
+    
+    eliminaProdotto(prod){ 
+        
+    }
 }
